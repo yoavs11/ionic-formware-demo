02c143f6e2fbb3dcdc6e494a89255faa456e3317
yoave11 <yoave11@yahoo.com>
2018-08-22 22:32:58 +0300

    ble send with response and without response

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,4 +1,4 @@
-import {Component, NgZone} from '@angular/core';
+import {ChangeDetectorRef, Component, NgZone} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {ToastController} from 'ionic-angular';
 import {BLE} from '@ionic-native/ble';
@@ -30,7 +30,7 @@ const parseReply = data => {
 }
 
 
-const inhalerInfoParser = (data) => {
+const inhalerInfoParser = (id, data) => {
   if (!data) {
     return {res: 'GOT NULL!'}
   }
@@ -54,6 +54,47 @@ const inhalerInfoParser = (data) => {
   }
 }
 
+const noopParser = (id: any, hexReply: any): any => parseReply(hexReply).payload;
+
+const chunkResponseParser = (id, data) => {
+  const {payloadLen, payload} = parseReply(data);
+  if (!payloadLen) {
+    throw new Error(`no reply from chunk sending`)
+  }
+
+  return {
+    success: payload[2] === 0
+  };
+}
+
+const nonZeroErrorParser = (id, data) => {
+  const {payloadLen, payload} = parseReply(data);
+  if (!payloadLen) {
+    throw new Error('No payload in inhaler reply to cmd 0x${hex(id)}. Expected an error value as payload[0]')
+  }
+  if (payload[0]) {
+    throw new Error(`Inhaler reaplied with error code 0x${hex(payload[0])} to cmd 0x${hex(id)}.`)
+  }
+  return payload;
+}
+
+const hex = v => {
+  if (Array.isArray(v)) {
+    return v.reduce((all, curr) => all + hex(curr), "");
+  }
+  const s = v.toString(16);
+  return s.length == 1 ? "0" + s : s;
+};
+
+const formatCommand = (id, payload = []) => {
+  const coreMsg = [id, payload.length, ...payload];
+  const checksum = coreMsg.reduce((all, curr) => all + curr) % 256;
+  const msg = coreMsg.concat([checksum]);
+  return new Uint8Array(msg)
+};
+
+const int16Bytes = v => [v >> 8, v & 255];
+
 @Component({
   selector: 'page-detail',
   templateUrl: 'detail.html',
@@ -62,13 +103,14 @@ export class DetailPage {
 
   peripheral: any = {};
   statusMessage: string;
-  deviceOutput = {};
+  inhalerInfo = {};
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private ble: BLE,
               public bleProvider: BleProvider,
               private toastCtrl: ToastController,
+              private changeDetection: ChangeDetectorRef,
               private ngZone: NgZone) {
 
     let device = navParams.get('device');
@@ -83,17 +125,18 @@ export class DetailPage {
   }
 
 
-  onConnected(peripheral) {
+  async onConnected(peripheral) {
     this.ngZone.run(() => {
       this.setStatus('');
       this.peripheral = peripheral;
+      this.bleProvider.createConnection(peripheral.id);
     });
 
-    this.bleProvider.createConnection(peripheral.id);
-    this.bleProvider.readValue().subscribe(readData => {
-      this.deviceOutput = inhalerInfoParser(readData);
-      console.log({result: this.deviceOutput});
-    });
+    this.inhalerInfo = await this.getInhalerInfo()
+    console.log({result: this.inhalerInfo});
+    this.changeDetection.detectChanges();
+    await this.testCmd();
+
   }
 
   onDeviceDisconnected(peripheral) {
@@ -102,7 +145,55 @@ export class DetailPage {
       duration: 3000,
       position: 'middle'
     });
+    this.bleProvider.ngOnDestroy();
     toast.present();
+  }
+
+  async testCmd() {
+    try {
+      console.log('starting');
+      const isChanged = await this.changeMode(true);
+      console.log({isChanged});
+      const isStartDownload = await this.startDownload();
+      console.log({isStartDownload})
+      const size = 50;
+      for (let i = 0; i < size; i++) {
+        console.log(`chunk ${i}`)
+        const chunk = Array(128).fill(i % 256)
+        await this.sendChunk(i, chunk);
+      }
+      console.log('finished');
+      const isUpdated = await this.updateFirmware();
+      console.log({isUpdated})
+    } catch (e) {
+      alert(e.message);
+    }
+  }
+
+  private async sendCommand(id, parser = noopParser, extraData = []) {
+    const res = await this.bleProvider.sendCommand(formatCommand(id, extraData));
+    return parser(id, res)
+  }
+
+  private getInhalerInfo() {
+    return this.sendCommand(0x01, inhalerInfoParser)
+  }
+
+  private async changeMode(isChangeToBoot: boolean) {
+    return this.sendCommand(0x02, noopParser, [isChangeToBoot ? 0x01 : 0x00])
+  }
+
+  private async startDownload() {
+    return this.sendCommand(0x03, nonZeroErrorParser)
+  }
+
+  private async sendChunk(chunckNum, chunckBytes) {
+    // return this.sendCommand(0x04, chunkResponseParser, [...int16Bytes(chunckNum), ...chunckBytes])
+    this.bleProvider.writeWithoutResponse(formatCommand(0x04, [...int16Bytes(chunckNum), ...chunckBytes]))
+  }
+
+  private async updateFirmware() {
+    return this.sendCommand(0x05, nonZeroErrorParser)
   }
 
   // Disconnect peripheral when leaving the page
--- src/providers/ble/ble.ts
+++ src/providers/ble/ble.ts
@@ -5,7 +5,17 @@ import {BehaviorSubject} from "rxjs/BehaviorSubject";
 import {Subscription} from "rxjs/Subscription";
 import 'rxjs/add/observable/empty';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/concatMap';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/concatAll';
+import 'rxjs/add/operator/toPromise';
 import {Observable} from "rxjs/Observable";
+import {ReplaySubject} from "rxjs/ReplaySubject";
+import {fromPromise} from 'rxjs/Observable/fromPromise';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+
 
 const serialPortServiceId = '0783B03E-8535-B5A0-7140-A304D2495CB7';
 const serialPortReadNotifyCharactaristicId = '0783b03e-8535-b5a0-7140-a304d2495cb8';
@@ -15,28 +25,20 @@ const serialPortFlowControlCharactaristicId = '0783b03e-8535-b5a0-7140-a304d2495
 @Injectable()
 export class BleProvider implements OnDestroy {
 
-  private inboundSubject: BehaviorSubject<any>;
-  private outboundSubject: BehaviorSubject<Uint8Array>;
+  private writeToInhalerWithoutResponseSubject: ReplaySubject<Uint8Array>;
 
-  private inboundSubscription: Subscription;
   private flowControlSubscription: Subscription;
   private deviceId: string;
 
 
   constructor(private ble: BLE) {
-    this.inboundSubject = new BehaviorSubject(null);
-    this.outboundSubject = new BehaviorSubject(new Uint8Array([0x01, 0x00, 0x01]));
+    this.writeToInhalerWithoutResponseSubject = new ReplaySubject();
   }
 
   public createConnection(deviceId) {
     this.deviceId = deviceId;
-    this.inboundSubscription = this.ble.startNotification(
-      this.deviceId,
-      serialPortServiceId,
-      serialPortReadNotifyCharactaristicId)
-      .subscribe(this.inboundSubject);
-
-    this.flowControlSubscription = this.ble.startNotification(
+    this.flowControlSubscription =
+      this.ble.startNotification(
       this.deviceId,
       serialPortServiceId,
       serialPortFlowControlCharactaristicId)
@@ -45,26 +47,45 @@ export class BleProvider implements OnDestroy {
           return Observable.empty();
         }
         const response = new Uint8Array(data);
-        return response[0] ? this.outboundSubject : Observable.empty();
+        console.log({flowStatus: response[0]})
+        return response[0] ? this.writeToInhalerWithoutResponseSubject : Observable.empty();
       })
-      .subscribe(
-        (data: Uint8Array) =>
-        this.ble.writeWithoutResponse(this.deviceId, serialPortServiceId, serialPortWriteCharactaristicId, data.buffer)
-      );
+        .map((value) => Observable.of(value).delay(50))
+        .concatAll()
+        .subscribe(async (data: Uint8Array) => {
+          if (data) {
+            await this.ble.writeWithoutResponse(this.deviceId, serialPortServiceId, serialPortWriteCharactaristicId, data.buffer)
+          }
+        })
   }
 
-  public async write(data: Uint8Array) {
-    this.outboundSubject.next(data);
+  public writeWithoutResponse(data: Uint8Array) {
+    this.writeToInhalerWithoutResponseSubject.next(data);
   }
 
-  public readValue() {
-    return this.inboundSubject.asObservable();
+  public async sendCommand(command: Uint8Array) {
+    return this.ble.startNotification(
+      this.deviceId,
+      serialPortServiceId,
+      serialPortFlowControlCharactaristicId)
+      .switchMap((data) => {
+        if (!data) {
+          return Observable.empty();
+        }
+        const response = new Uint8Array(data);
+        console.log({flowStatus: response[0]})
+        return response[0] ? fromPromise(this.ble.writeWithoutResponse(this.deviceId, serialPortServiceId, serialPortWriteCharactaristicId, command.buffer)) : Observable.empty();
+      })
+      .switchMap(() => this.ble.startNotification(
+        this.deviceId,
+        serialPortServiceId,
+        serialPortReadNotifyCharactaristicId))
+      .take(1).toPromise();
   }
+
 
   ngOnDestroy(): void {
-    if (this.inboundSubscription) {
-      this.inboundSubscription.unsubscribe();
-    }
+    console.log('unsubscribe')
     if (this.flowControlSubscription) {
       this.flowControlSubscription.unsubscribe();
     }
